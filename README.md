411411c4a27eb04daeea3a352995067005918bd9
TheSkibb <kfskibrek@icloud.com>
2022-11-02 08:57:49 +0100

    initial commit of server code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # expressJS-and-mariaDB-connection
 example of how a expressJS and mariaDB could look
+
+"npm run run" to tun the project
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const mysql = require('mysql')
+const app = express();
+const port = 3002;
+
+let connection = mysql.createConnection({
+    host: '172.17.0.2',
+    user: 'hotellrommet',
+    password: '123',
+    database: 'hotellrommet'
+})
+
+function getAll(){
+  try{
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * from demoTable", (err, result)=>{
+            if(err){
+                // The equivalent of throwing the error
+                reject(err);
+            } else {
+                // The equivalent of returning a value for getAll
+                resolve(result[0].testValue);
+            }
+        })
+    })
+  }
+  catch(err){
+    throw 'error while querying database'
+  }
+}
+
+async function print(){
+  console.log('test', await getAll())
+}
+print()
+
+app.listen(port, () => {
+  console.log("server running on port", port);
+});
+
+app.get("/", async (req, res) => {
+  console.log('received get request')
+  try{
+    const result = await getAll()
+    res.send({
+      test: result
+    })
+  }
+  catch(err){
+    const errorMsg = "some error ocurred" + err;
+    res.send(
+      errorMsg
+    )
+  }
+});
